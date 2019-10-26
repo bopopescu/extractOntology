fbafd4448e6e2581601b290eecb37b6838aabe90
kangqiwang <kangqiwang@outlook.com>
2019-10-26 06:25:58 +0100

    update some jobs to build website

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import render_template,flash,request
+
+@app.route('/')
+@app.route('/index')
+@app.route('/upload', methods=['GET','POST'])
+
+def upload_file():
+    if request.method=='POST':
+        f=request.files['file']
+        print('I SAVE FILE')
+        return 'file uploaded successfully'
+    else:
+        return render_template('upload.html')
+def index():
+    user={'username':'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html',title='Home',user=user, posts=posts)
