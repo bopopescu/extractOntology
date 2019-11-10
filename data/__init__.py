0da4f7937508423d4da73d4a0c81b5e6dcaf9865
kangqiwang <kangqiwang@outlook.com>
2019-11-10 20:38:51 +0000

    update graph html

--- data/__init__.py
+++ data/__init__.py
@@ -4,9 +4,16 @@
 # # print(output)
 import pandas as pd
 from py2neo import *
+import math
+def convertToNumber (s):
+    return int.from_bytes(s.encode(), 'little')
+
+def convertFromNumber (n):
+    return n.to_bytes(math.ceil(n.bit_length() / 8), 'little').decode()
+
 graph = Graph(uri="http://127.0.0.1:7474/", username="neo4j", password="Wnwwykcj6")
 graph.delete_all()
-tx = graph.begin()
+
 
 df= pd.read_csv("data/CSO.3.1.csv",header=None)
 for index,data in df.iteritems():
@@ -17,13 +24,19 @@ for index,data in df.iteritems():
     df[index] = df[index].str.replace("-", "_")
     df[index] = df[index].str.extract(r".*\/(\w+(?:\.\w+)?)[?\.]?")
 for index,row in df.iterrows():
+    tx = graph.begin()
     node1=Node('Ontology',name=row[0])
     node2=Node('Ontology',name=row[1])
     relation=Relationship(node1,row[2],node2)
-    tx.create(node1)
-    tx.create(node2)
+    if index==0:
+        tx.create(node1)
+        tx.create(node2)
+    else:
+        tx.merge(node1)
+        tx.merge(node2)
+    
     tx.create(relation)
     print(index)
-tx.commit()
+    tx.commit()
 
 print("finished load data")
